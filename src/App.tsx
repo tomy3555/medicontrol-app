324fdcf4d6d546514ca9fa3fa1ef9cb68166ab97
tomy3555 <tomilagomar@gmail.com>
2026-03-11 13:50:56 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+
+import { LoginPage } from './pages/LoginPage';
+
+export const App = () => {
+  return (
+    <LoginPage/>
+  )
+}
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,59 @@
+import { useLogin } from "../hooks/useLogin";
+
+
+
+export const LoginPage = () => {
+
+  const{ email, password, handleEmailChange, handlePasswordChange, handleLogin } = useLogin();
+
+  return (
+     <div className="min-h-screen flex items-center justify-center bg-gray-100"> {/* color de fondo y layout */}
+      <div className="w-full max-w-md bg-white p-8 rounded-xl shadow-md space-y-6">
+        
+        <div className="text-center">
+          <h1 className="text-3xl font-semibold text-blue-600">MediControl</h1>
+          <p className="text-gray-500 text-sm mt-1">
+            Maneja tu salud
+          </p>
+        </div>
+
+        <div className="space-y-4">
+          <div>
+            <label className="block text-sm text-gray-700 mb-1">
+              Email
+            </label>
+            <input
+              type="email"
+              value={email}
+              placeholder="Ingresar email"
+              className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              onChange={handleEmailChange}
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm text-gray-700 mb-1">
+              Password
+            </label>
+            <input
+              type="password"
+              value={password}
+              placeholder="Ingresar contraseña"
+              className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              onChange={handlePasswordChange}
+            />
+          </div>
+        </div>
+
+        <button className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition"
+        onClick={handleLogin}>
+          Iniciar Sesion
+        </button>
+
+        <p className="text-center text-sm text-blue-600 cursor-pointer hover:underline">
+          Crear Cuenta
+        </p>
+      </div>
+    </div>
+  )
+}
